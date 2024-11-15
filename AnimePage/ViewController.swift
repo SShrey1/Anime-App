3645f77fc5fe0267c17d9bbbcb1d438cc8e080b3
SShrey1 <palishrey@gmail.com>
2024-11-15 21:19:17 +0530

    Initial Commit

---
+++ AnimePage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimePage
+//
+//  Created by user@59 on 24/08/1946 Saka.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AnimePageTests/AnimePageTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimePageTests.swift
+//  AnimePageTests
+//
+//  Created by user@59 on 24/08/1946 Saka.
+//
+
+import Testing
+@testable import AnimePage
+
+struct AnimePageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
